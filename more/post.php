bda815691307e5e999def324b5b53bc37d20f3cb
Julien Kirch <archiloque@archiloque.net>
2009-10-29 19:56:41 +0100

    Sécurisation

--- more/post.php
+++ more/post.php
@@ -1,6 +1,16 @@
 <?php
 
-$base_path = "images/"; 
+function endsWith($haystack, $needle, $case=true) {
+    if($case){return (strcmp(substr($haystack, strlen($haystack) - strlen($needle)),$needle)===0);}
+    return (strcasecmp(substr($haystack, strlen($haystack) - strlen($needle)),$needle)===0);
+}
+
+if(!endsWith(gethostbyaddr($_SERVER["REMOTE_ADDR"]), "amazonaws.com") {
+         header($_SERVER["SERVER_PROTOCOL"]. "Wük ?", true, 500);
+         die("Wük ?");
+}
+
+$base_path = "images/";
 
 if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
    $target = $base_path . $_GET['id'];
@@ -17,11 +27,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
 } else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
    if ($_POST['id'] == '') {
       header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-      echo "No id"; 
+      echo "No id";
    } else if ($_FILES['content']['size'] > 500000) {
       header($_SERVER["SERVER_PROTOCOL"]." 400 Bad Request");
-      echo "Your file is too large"; 
-   } else { 
+      echo "Your file is too large";
+   } else {
       $target = $base_path . $_POST['id'];
       if(file_exists($target)) {
          if(!unlink($target)) {
@@ -29,12 +39,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
             die();
          }
       }
-      if(move_uploaded_file($_FILES['content']['tmp_name'], $target)) { 
-         echo "OK"; 
-      } else { 
+      if(move_uploaded_file($_FILES['content']['tmp_name'], $target)) {
+         echo "OK";
+      } else {
          header($_SERVER["SERVER_PROTOCOL"]. "Failed to copy the file", true, 500);
-      } 
-   } 
+      }
+   }
 } else {
    echo "WTF?";
 }
