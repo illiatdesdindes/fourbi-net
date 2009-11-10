d263fb5c822cd721dc72a4c858606d886379e318
Julien Kirch <archiloque@archiloque.net>
2009-11-10 19:53:05 +0100

    stupid typo

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
-Paperclip::Storage::Http::SCRIPT_URL = ENV['PAPERCLIP_SCRIPT_URL'],
+Paperclip::Storage::Http::SCRIPT_URL = ENV['PAPERCLIP_SCRIPT_URL']
 Paperclip::Storage::Http::IMAGES_ROOT_URL = ENV['PAPERCLIP_IMAGES_ROOT_URL']
 Paperclip::Storage::Http::USER = ENV['PAPERCLIP_USER']
 Paperclip::Storage::Http::PASSWORD = ENV['PAPERCLIP_PASSWORD']
