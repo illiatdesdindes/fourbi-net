980c2a98c52e44ca4febd2e6c08cb515fd869a07
Julien Kirch <code@archiloque.net>
2010-04-08 19:57:37 +0200

    fixed image size calculation

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -1,29 +1,21 @@
 $(function() {
-    for (var i = 1; i <= 3; i++) {
+    for (var i = 0; i < 3; i++) {
         resize_to_fit($("#sommaire_image_desordre_" + i + "> img"), $("#sommaire_image_desordre_" + i));
     }
-    for (var j = 1; j <= 4; j++) {
+    for (var j = 0; j < 4; j++) {
         resize_to_fit($("#sommaire_image_terrier_" + j + "> img"), $("#sommaire_image_terrier_" + j));
     }
 });
 
-function resize_to_fit(source, target) {
-    var source_h = source.height();
-    var source_w = source.width();
-
-    var parent_h = target.height();
-    var parent_w = target.width();
-
-    var ratio_source = source_h / source_w;
-    var ratio_target = parent_h / parent_w;
+function resize_to_fit(image, target_frame) {
 
     var delta;
-    if (ratio_source < ratio_target) {
-        delta = parent_h - (source_h * ( parent_w / source_w));
-        source.css({'width': parent_w, 'position': 'absolute', 'top': (delta / 2)});
+    if ((target_frame.height() / image.height() * image.width()) >= target_frame.width()) {
+        delta = (target_frame.height() - (target_frame.width() / image.width() * image.height() )) / 2;
+        image.css({'width': target_frame.width(), 'position': 'absolute', 'top': delta });
     } else {
-        delta = parent_w - (source_w * ( parent_h / source_h));
-        source.css({'height': parent_h, 'position': 'absolute', 'left': (delta / 2)});
+        delta = (target_frame.width() - (target_frame.height() / image.height() * image.width() )) / 2;
+        image.css({'height': target_frame.height(), 'position': 'absolute', 'left': delta });
     }
 
 }
