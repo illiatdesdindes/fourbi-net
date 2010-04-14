d20de3f6f3184f2e7e2754dc753c00013d2ef253
Julien Kirch <code@archiloque.net>
2010-04-14 19:26:04 +0200

    correction de mise ne page des articles

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -1,3 +1,10 @@
+$(function() {
+   if($("#article_desordre_image").width() > $("#article_desordre_predelle").width()) {
+        $("#article_desordre_predelle").css("padding-left", "" + (($("#article_desordre_image").width() - $("#article_desordre_predelle").width()) / 2) + "px" );
+   }
+});
+
+
 function scollLeft() {
     $("#article_scroll_content").scrollTo( "-=" + (numberArticlesPerPage * $($("#article_scroll_content td")[0]).outerWidth()), 800);
 }
