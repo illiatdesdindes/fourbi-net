ce225d819d57700c59102e263dc6031bc07d6502
Julien Kirch <code@archiloque.net>
2010-04-14 19:46:46 +0200

    centrage de l'image quand elle est chargée

--- public/javascripts/public.js
+++ public/javascripts/public.js
@@ -1,14 +1,16 @@
 $(function() {
-   if($("#article_desordre_image").width() > $("#article_desordre_predelle").width()) {
-        $("#article_desordre_predelle").css("padding-left", "" + (($("#article_desordre_image").width() - $("#article_desordre_predelle").width()) / 2) + "px" );
-   }
+    $("#article_desordre_image").load(function() {
+        if ($("#article_desordre_image").width() > $("#article_desordre_predelle").width()) {
+            $("#article_desordre_predelle").css("padding-left", "" + (($("#article_desordre_image").width() - $("#article_desordre_predelle").width()) / 2) + "px");
+        }
+    });
 });
 
 
 function scollLeft() {
-    $("#article_scroll_content").scrollTo( "-=" + (numberArticlesPerPage * $($("#article_scroll_content td")[0]).outerWidth()), 800);
+    $("#article_scroll_content").scrollTo("-=" + (numberArticlesPerPage * $($("#article_scroll_content td")[0]).outerWidth()), 800);
 }
 
 function scollRight() {
-    $("#article_scroll_content").scrollTo( "+=" + (numberArticlesPerPage * $($("#article_scroll_content td")[0]).outerWidth()), 800);
+    $("#article_scroll_content").scrollTo("+=" + (numberArticlesPerPage * $($("#article_scroll_content td")[0]).outerWidth()), 800);
 }
