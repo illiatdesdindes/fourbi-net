eee55d03cdf776a0089854556e06144ee6d8581e
Julien Kirch <code@archiloque.net>
2010-04-17 12:53:54 +0200

    nouveau changement de taille des images

--- app/controllers/public/index_controller.rb
+++ app/controllers/public/index_controller.rb
@@ -29,17 +29,19 @@ class Public::IndexController < Public::DefaultPublicController
   end
 
   def serie_desordre
-
+    @serie = Serie.find(params[:id])
+    @page_title = "fourbi.net: #{@serie.nom}"
   end
 
   def serie_terrier
-
+    @serie = Serie.find(params[:id])
+    @page_title = "fourbi.net: #{@serie.nom}"
   end
 
   def sommaire
     @page_title = 'fourbi.net, brocante à toutes heures'
     boutique_desordre = Boutique.nom(Boutique::NOM_DESORDRE).first
-    @articles_desordre = find_articles 3, boutique_desordre
+    @articles_desordre = find_articles 5, boutique_desordre
     boutique_terrier = Boutique.nom(Boutique::NOM_TERRIER).first
     @articles_terrier = find_articles 4, boutique_terrier
   end
