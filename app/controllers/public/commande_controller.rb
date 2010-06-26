7a7ff0a3706fdefdf19d3c11b0ae5bbec8ed6a4f
Julien Kirch <code@archiloque.net>
2010-06-26 23:51:52 +0200

    post pour modification panier

--- app/controllers/public/commande_controller.rb
+++ app/controllers/public/commande_controller.rb
@@ -8,22 +8,26 @@ class Public::CommandeController < Public::DefaultPublicController
 
   def panier
     @panier = session[:panier] || []
-    if params[:add]
-      article = Article.find(params[:add].to_i)
-      if article
-        @panier << article.id
-        session[:panier] = @panier
-        redirect_to({:action => :panier}, {:notice => "L'article \"#{article.nom}\" a été ajouté à votre panier"})
+    if request.post?
+      if params[:add]
+        article = Article.find(params[:add].to_i)
+        if article
+          @panier << article.id
+          session[:panier] = @panier
+          redirect_to({:action => :panier}, {:notice => "L'article \"#{article.nom}\" a été ajouté à votre panier"})
+        else
+          redirect_to({:action => :panier}, {:alert => 'Article non trouvé'})
+        end
+      elsif params[:remove]
+        if i = @panier.index(params[:remove].to_i)
+          @panier.delete_at i
+          session[:panier] = @panier
+          redirect_to({:action => :panier}, {:notice => 'L\'article a été supprimé de votre panier'})
+        else
+          redirect_to({:action => :panier}, {:alert => 'Article non trouvé'})
+        end
       else
-        redirect_to({:action => :panier}, {:alert => 'Article non trouvé'})
-      end
-    elsif params[:remove]
-      if i = @panier.index(params[:remove].to_i)
-        @panier.delete_at i
-        session[:panier] = @panier
-        redirect_to({:action => :panier}, {:notice => 'L\'article a été supprimé de votre panier'})
-      else
-        redirect_to({:action => :panier}, {:alert => 'Article non trouvé'})
+        redirect_to({:action => :panier})
       end
     else
       @custom_css = 'body { background-image: url("/images/fonds/panier.jpg"); }'
