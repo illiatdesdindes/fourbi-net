d9abe3966880d92be9ecf4bd5b1cb1ca4cf740da
Julien Kirch <code@archiloque.net>
2010-07-12 20:49:15 +0200

    reconstruction des pages d'articles

--- db/migrate/20100708181921_add_descriptif_court.rb
+++ db/migrate/20100708181921_add_descriptif_court.rb
@@ -1,11 +1,11 @@
 class AddDescriptifCourt < ActiveRecord::Migration
   def self.up
-    add_column :articles, :description_courte, :text , :null => true
-    rename_column :articles, :description, :description_longue
+    add_column :articles, :description_longue, :text , :null => true
+    rename_column :articles, :description, :description_courte
   end
 
   def self.down
-    remove_column :articles, :description_courte
-    rename_column :articles, :description_longue, :description
+    remove_column :articles, :description_longue
+    rename_column :articles, :description_courte, :description
   end
 end
