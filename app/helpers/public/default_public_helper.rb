f447f5c82966ced1f5023fe170248c989238eafa
Julien Kirch <code@archiloque.net>
2010-07-18 11:53:01 +0200

    les zooms sont des popups

--- app/helpers/public/default_public_helper.rb
+++ app/helpers/public/default_public_helper.rb
@@ -8,4 +8,15 @@ module Public::DefaultPublicHelper
     "questions/#{sprintf('%03d', (rand(3) + 1))}.jpg"
   end
 
+  def javascript_pop_up url, name = 'popup', params = {}
+    {:location => false, :menubar => false, :personalbar => false, :titlebar => false}.each_pair do |key, value|
+      params[key] ||= value
+    end
+    params_array = []
+    params.each_pair do |key, value|
+      params_array << ["#{key.to_s}=#{value.to_s}"]
+    end
+    raw "window.open('#{url}', '#{name}', '#{params_array.join(',')}');"
+  end
+
 end
