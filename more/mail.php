0e72b5c897835ae88943da86f500ec1ea379ef49
Julien Kirch <code@archiloque.net>
2010-11-22 19:37:03 +0100

    text plain pour les mails

--- more/mail.php
+++ more/mail.php
@@ -1,11 +1,11 @@
 <?php
 
 function hop($mailFrom, $mailTo, $subject, $body) {
-	$headers  = "Content-Type:text/html;charset=utf-8\n";
+	$headers  = "Content-Type:text/plain;charset=utf-8\n";
 	$headers .= "Content-Transfer-Encoding: 8bit\n";
 	$headers .= "From: ".$mailFrom."\n";
 	return ( mail($mailTo, $subject, $body, $headers) );
 }
 
-hop( $_POST['mailFrom'], $_POST['mailTo'], $_POST['subject'], $_POST['body']);
+return hop( $_POST['mailFrom'], $_POST['mailTo'], $_POST['subject'], $_POST['body']);
 ?>
