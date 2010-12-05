4c912e331d7d7c74c5793af2f43a0186b8b8f8d0
Julien Kirch <code@archiloque.net>
2010-12-05 12:54:15 +0100

    zoom sous IE

--- app/helpers/public/default_public_helper.rb
+++ app/helpers/public/default_public_helper.rb
@@ -8,13 +8,12 @@ module Public::DefaultPublicHelper
     "questions/#{sprintf('%03d', (rand(3) + 1))}.jpg"
   end
 
-  def javascript_pop_up url, name = 'popup', params = {}
+  def javascript_pop_up url, params = {}
     {:location => false, :menubar => false, :personalbar => false, :titlebar => false, :resizable => true, :scrollbars => true}.each_pair do |key, value|
       params[key] ||= value
     end
     params_array = []
     params.each_pair do |key, value|
-      value_text = value
       if value.is_a? TrueClass
         value_text = 'yes'
       elsif value.is_a? TrueClass
@@ -24,7 +23,7 @@ module Public::DefaultPublicHelper
       end
       params_array << ["#{key.to_s}=#{value_text}"]
     end
-    raw "window.open('#{url}', '#{name}', '#{params_array.join(',')}');"
+    raw "javascript:window.open('#{url}', 'Zoom', '#{params_array.join(',')}');"
   end
 
 end
