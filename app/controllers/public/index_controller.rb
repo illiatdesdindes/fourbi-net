531df1765601b862b1ec4ec2df1c9c26ee9f1edc
Julien Kirch <code@archiloque.net>
2011-03-06 15:58:42 +0100

    news on index page

--- app/controllers/public/index_controller.rb
+++ app/controllers/public/index_controller.rb
@@ -21,7 +21,7 @@ class Public::IndexController < Public::DefaultPublicController
     @articles_desordre = find_articles 5, boutique_desordre
     boutique_terrier = Boutique.nom(Boutique::NOM_TERRIER).first
     @articles_terrier = find_articles 4, boutique_terrier
-    @nouveautes = Article.nouveaute(3)
+    @nouveautes = Article.nouveaute(3).includes(:serie => :boutique)
   end
 
 end
