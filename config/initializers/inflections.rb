ca5c6feecb084571604ce08dc609dc73ff24b3eb
Julien Kirch <code@archiloque.net>
2011-03-06 18:06:55 +0100

    correction du pluriel de metas

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,9 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-# Add new inflection rules using the following format 
+# Add new inflection rules using the following format
 # (all these examples are active by default):
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'serie', 'series'
+  inflect.irregular 'meta', 'metas'
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
